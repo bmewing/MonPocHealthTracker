7cb17f3b10f2138ee76f560d629416b48e710399
Ewing <u772700@emn.com>
2020-02-24 15:37:26 -0500

    fully working example

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 import dash
 import dash_core_components as dcc
@@ -7,29 +8,37 @@ from dash.dependencies import Input, Output
 
 from PIL import Image, ImageDraw, ImageFont
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+path = ''
+
+with open('monsters.json', 'r') as monster_names_file:
+    monster_data = json.load(monster_names_file)
+monster_names = [{'label': m['name'], 'value': m['name']} for m in monster_data]
+monster_names.append({'label': 'None', 'value': ''})
 
 app = dash.Dash(__name__)
 
 
 def gen_monster(player, i):
-    name = 'Monster #%d' % i
+    name = 'Monster #%d:  ' % i
     id_gen = '%s-mon%d' % (player, i)
     output = html.Div([
-        html.Label('%s Name' % name),
-        dcc.Input(value='', type='text', id='%s-name' % id_gen),
-        html.Label('%s Health' % name),
+        html.Label('%s Name   ' % name),
+        html.Br(),
+        dcc.Dropdown(
+            options=monster_names,
+            value='',
+            id='%s-name' % id_gen
+        ),
+        html.Br(),
+        html.Label('Health'),
+        html.Br(),
         dcc.Input(value=11, type='number', id='%s-health' % id_gen),
-        html.Label('%s State' % name),
-        dcc.RadioItems(
-            options=[
-                {'label': 'Alpha', 'value': 'alpha'},
-                {'label': 'Hyper', 'value': 'hyper'}
-            ],
-            value='alpha',
-            id='%s-state' % id_gen
-        )
-    ], style={'border': 'dotted 1px black'})
+        html.Br(),
+        html.Label('Hyper Transition'),
+        html.Br(),
+        dcc.Input(value=6, type='number', id='%s-state' % id_gen),
+    ], style={'border': 'dotted 1px black',
+              'padding': '10px'})
     return output
 
 
@@ -38,8 +47,10 @@ app.layout = html.Div(children=[
 
     html.Div([
         html.Div([
-            html.Label('Left Player Name'),
+            html.Label('Left Player Name: '),
+            html.Br(),
             dcc.Input(value='Mark', type='text', id='lp-name'),
+            html.P(),
             gen_monster('lp', 1),
             html.Br(),
             gen_monster('lp', 2),
@@ -50,8 +61,10 @@ app.layout = html.Div(children=[
         html.Br(),
 
         html.Div([
-            html.Label('Right Player Name'),
-            dcc.Input(value='Benjamin', type='text'),
+            html.Label('Right Player Name: '),
+            html.Br(),
+            dcc.Input(value='Benjamin', type='text', id='rp-name'),
+            html.P(),
             gen_monster('rp', 1),
             html.Br(),
             gen_monster('rp', 2),
@@ -60,7 +73,8 @@ app.layout = html.Div(children=[
         ], style={'width': '300px'})
     ], style={'columnCount': 2}),
 
-    html.Div(id='dummy')
+    html.Div(id='dummy1'),
+    html.Div(id='dummy2')
 ])
 
 # one image for the left player
@@ -68,10 +82,155 @@ app.layout = html.Div(children=[
 # write out transparent image of fixed size for 1/2/3 monster game
 # write out real image for whatever size game is being played
 # that way, images are always positioned correctly
+# longest monster name is Sergeant Titanica (17 characters)
 
 
 @app.callback(
-    Output('dummy', 'children'),
+    Output('rp-mon1-health', 'value'),
+    [Input('rp-mon1-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['health']
+    else:
+        return 11
+
+
+@app.callback(
+    Output('rp-mon1-state', 'value'),
+    [Input('rp-mon1-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['transition']
+    else:
+        return 6
+
+
+@app.callback(
+    Output('rp-mon2-health', 'value'),
+    [Input('rp-mon2-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['health']
+    else:
+        return 11
+
+
+@app.callback(
+    Output('rp-mon2-state', 'value'),
+    [Input('rp-mon2-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['transition']
+    else:
+        return 6
+
+
+@app.callback(
+    Output('rp-mon3-health', 'value'),
+    [Input('rp-mon3-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['health']
+    else:
+        return 11
+
+
+@app.callback(
+    Output('rp-mon3-state', 'value'),
+    [Input('rp-mon3-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['transition']
+    else:
+        return 6
+
+
+@app.callback(
+    Output('lp-mon1-health', 'value'),
+    [Input('lp-mon1-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['health']
+    else:
+        return 11
+
+
+@app.callback(
+    Output('lp-mon1-state', 'value'),
+    [Input('lp-mon1-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['transition']
+    else:
+        return 6
+
+
+@app.callback(
+    Output('lp-mon2-health', 'value'),
+    [Input('lp-mon2-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['health']
+    else:
+        return 11
+
+
+@app.callback(
+    Output('lp-mon2-state', 'value'),
+    [Input('lp-mon2-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['transition']
+    else:
+        return 6
+
+
+@app.callback(
+    Output('lp-mon3-health', 'value'),
+    [Input('lp-mon3-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['health']
+    else:
+        return 11
+
+
+@app.callback(
+    Output('lp-mon3-state', 'value'),
+    [Input('lp-mon3-name', 'value')]
+)
+def update_health(monster):
+    if monster != '':
+        relevant = [m for m in monster_data if m['name'] == monster]
+        return relevant[0]['transition']
+    else:
+        return 6
+
+
+@app.callback(
+    Output('dummy1', 'children'),
     [Input('lp-mon1-name', 'value'),
      Input('lp-mon1-health', 'value'),
      Input('lp-mon1-state', 'value'),
@@ -80,8 +239,24 @@ app.layout = html.Div(children=[
      Input('lp-mon2-state', 'value'),
      Input('lp-mon3-name', 'value'),
      Input('lp-mon3-health', 'value'),
-     Input('lp-mon3-state', 'value'),
-     Input('rp-mon1-name', 'value'),
+     Input('lp-mon3-state', 'value')
+     ]
+)
+def gen_image(lp1name, lp1health, lp1state,
+              lp2name, lp2health, lp2state,
+              lp3name, lp3health, lp3state
+              ):
+    lp = [{'name': lp1name, 'health': lp1health, 'state': det_state(lp1health, lp1state)},
+          {'name': lp2name, 'health': lp2health, 'state': det_state(lp2health, lp2state)},
+          {'name': lp3name, 'health': lp3health, 'state': det_state(lp3health, lp3state)}]
+    lp = [m for m in lp if m['name'] != '']
+    gen_monster_img(lp, 'left')
+    return datetime.datetime.now()
+
+
+@app.callback(
+    Output('dummy2', 'children'),
+    [Input('rp-mon1-name', 'value'),
      Input('rp-mon1-health', 'value'),
      Input('rp-mon1-state', 'value'),
      Input('rp-mon2-name', 'value'),
@@ -94,33 +269,70 @@ app.layout = html.Div(children=[
 )
 def gen_image(lp1name, lp1health, lp1state,
               lp2name, lp2health, lp2state,
-              lp3name, lp3health, lp3state,
-              rp1name, rp1health, rp1state,
-              rp2name, rp2health, rp2state,
-              rp3name, rp3health, rp3state
+              lp3name, lp3health, lp3state
               ):
-    lp = [{'name': lp1name, 'health': lp1health, 'state': lp1state},
-          {'name': lp2name, 'health': lp2health, 'state': lp2state},
-          {'name': lp3name, 'health': lp3health, 'state': lp3state}]
-    rp = [{'name': rp1name, 'health': rp1health, 'state': rp1state},
-          {'name': rp2name, 'health': rp2health, 'state': rp2state},
-          {'name': rp3name, 'health': rp3health, 'state': rp3state}]
+    lp = [{'name': lp1name, 'health': lp1health, 'state': det_state(lp1health, lp1state)},
+          {'name': lp2name, 'health': lp2health, 'state': det_state(lp2health, lp2state)},
+          {'name': lp3name, 'health': lp3health, 'state': det_state(lp3health, lp3state)}]
     lp = [m for m in lp if m['name'] != '']
-    rp = [m for m in rp if m['name'] != '']
-    if len(lp) == len(rp):
-        img = Image.new('RGBA', (800, 200), color=(255, 255, 255, 0))
-        fnt = ImageFont.truetype('C://Windows//Fonts//BAUHS93.ttf', 24)
-        d = ImageDraw.Draw(img)
-        for lm in range(len(lp)):
-            d.text((10, 50 + 100*lm),
-                   '%s: %d' % (lp[lm]['name'], lp[lm]['health']),
-                   font=fnt, fill=(0, 0, 0))
-        for rm in range(len(rp)):
-            d.text((410, 50 + 100*rm),
-                   '%s: %d' % (rp[rm]['name'], rp[rm]['health']),
-                   font=fnt, fill=(0, 0, 0))
-        img.save('trial.png')
+    gen_monster_img(lp, 'right')
     return datetime.datetime.now()
+
+
+def det_state(health, transition):
+    if health <= transition:
+        return "hyper"
+    else:
+        return "alpha"
+
+
+def gen_monster_img(monsters, player):
+    i = len(monsters)
+    width = 400
+    mon_fnt = ImageFont.truetype('font/MAL_DE_OJO.ttf', 48)
+    hlt_fnt = ImageFont.truetype('font/HEADLINER.ttf', 60)
+
+    text_color = {
+        'alpha': (9, 0, 171),
+        'hyper': (163, 158, 0)
+    }
+
+    height1 = 84
+    height2 = 137
+    height3 = 190
+
+    img1 = Image.new('RGBA', (width, height1), color=(255, 255, 255, 0))
+    img2 = Image.new('RGBA', (width, height2), color=(255, 255, 255, 0))
+    img3 = Image.new('RGBA', (width, height3), color=(255, 255, 255, 0))
+
+    if i == 1:
+        height = height1
+        img1 = Image.new('RGB', (width, height), color=(166, 0, 22))
+        d = ImageDraw.Draw(img1)
+    elif i == 2:
+        height = height2
+        img2 = Image.new('RGB', (width, height), color=(166, 0, 22))
+        d = ImageDraw.Draw(img2)
+    elif i == 3:
+        height = height3
+        img3 = Image.new('RGB', (width, height), color=(166, 0, 22))
+        d = ImageDraw.Draw(img3)
+    else:
+        return ''
+    d.rectangle((10, 10, width-10, height-10), fill=(230, 209, 214), outline=(191, 124, 36))
+
+    for lm in range(len(monsters)):
+        tc = text_color[monsters[lm]['state']]
+        d.text((20, 20 + 53 * lm),
+               '%s:' % monsters[lm]['name'],
+               font=mon_fnt, fill=tc)
+        d.text((357, 20 + 50 * lm),
+               str(monsters[lm]['health']),
+               font=hlt_fnt, fill=(0, 0, 0))
+
+    img1.save(path+player+'_1.png')
+    img2.save(path+player+'_2.png')
+    img3.save(path+player+'_3.png')
 
 
 if __name__ == '__main__':
